f479fbfa24a8a828e0cbab9e206e2daeaee97e15
unknown <aavishlilani090304@gmail.com>
2025-02-18 13:47:30 +0530

    Login module

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,8 +2,13 @@ import { Hono } from 'hono'
 import { cors } from 'hono/cors';
 import { authRouter } from './routes/authRoutes';
 
-const app = new Hono()
-app.use("*",cors());
-app.route("/auth/",authRouter)
+const app = new Hono<{
+  Bindings:{
+    DATABASE_URL:string,
+    JWT_SECRET:string
+  }
+}>()
+app.use("*",cors())
+app.route("/auth",authRouter)
 
 export default app
--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,11 +1,65 @@
 import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { sign } from "hono/jwt";
 
-export const authRouter = new Hono();
-authRouter.get("/signin",async(c)=>{
-    const body= await c.req.json()
-    console.log(body)
-    return c.json(body)
-})
-authRouter.post("/signup",async(c)=>{
-    return c.text("Signup route")
-})
+export const authRouter = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+}>();
+
+authRouter.post("/signup", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  })
+ try{
+  const body = await c.req.json();
+  const userExists = await prisma.user.findFirst({
+    where:{
+        email:body.email
+    }
+  })
+  if(userExists){
+    return c.json({Error:"User Already Exists"},411)
+  }
+  const newUser = await prisma.user.create({
+    data:{
+        email:body.email,
+        username:body.username,
+        password:body.password
+    }
+  })
+  if(newUser){
+    const token = await sign({id:newUser.id,email:newUser.email
+    },c.env.JWT_SECRET)
+    return c.json({msg:"User Signed up Successfully",token})
+  }
+ }catch(error){
+    return c.json({error})
+ }
+ 
+});
+
+authRouter.post("/signin", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  })
+  try {
+    const body = await c.req.json();
+   
+    const user = await prisma.user.findFirst({
+      where: { email: body.email, password: body.password },
+    });
+    if (!user) {
+      return c.json({ error: "Invalid credentials" });
+    }
+    const token = await sign(
+      { id: user.id, username: user.email, name: user.username },
+      c.env.JWT_SECRET
+    );
+    return c.json({ token: token });
+  } catch (error) {
+    return c.json({ error:"Some error" });
+  }
+});
